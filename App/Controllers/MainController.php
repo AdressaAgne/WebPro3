183e65339fbb3ccb52a457725a60593742836628
thomhess <thomhess@Thomass-MacBook.local>
2016-10-26 12:48:16 +0200

    Like morsom som Mia

--- App/Controllers/MainController.php
+++ App/Controllers/MainController.php
@@ -14,7 +14,7 @@ class MainController extends BaseController {
     
     public function test(){
         return View::make('index', [
-            'food' => ['fisk', 'laks', 'mat', 'ball', 'mia']
+            'food' => ['fisk', 'laks', 'mat', 'ball', 'mia', 'thomas']
         ]);
     }
     
