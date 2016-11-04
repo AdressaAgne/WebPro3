8a5684f4ad945b01bf981ff52ef4dc6800e17dd4
thomhess <hessthom@gmail.com>
2016-11-04 15:14:50 +0100

    Loads of stuff committed, approved by Agne

---
+++ build/App/Helpers/Fractions.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Helpers;
+
+
+
+//Fraction Conversion
+class Fractions {
+    public static function numberWithDecimals_to_string($decimalNumber){
+
+        // Round to quarter above
+        $x = $decimalNumber * 4;
+        $y = ceil($x);
+        $roundedToQuarter = $y / 4;
+
+        // Split number and fraction
+        $wholeNumber = floor($roundedToQuarter);
+        $onlyDecimals = $roundedToQuarter - $wholeNumber;
+
+        // Make fraction into string
+        switch ($onlyDecimals) {
+            case 0.25:
+                $fraction = "¼";
+                break;
+
+            case 0.50:
+                $fraction = "½";
+                break;
+
+            case 0.75:
+                $fraction = "¾";
+                break;
+
+            default:
+                $fraction = "";
+                break;
+        }
+
+        // Return whole number and fraction
+        if ($wholeNumber == 0) {
+          return $fraction;
+
+        } else {
+          return $wholeNumber . " " . $fraction;
+        }
+    }
+}
---
+++ build/view/layout/recipie.php
@@ -0,0 +1,5 @@
+<article class="col-12">
+	<div class="col-12 recipie-preview" style="background-image: url('/assets/img/recipis/{{$res['image']}}');"></div>
+	<h1>{{ $res['name']}}</h1>
+
+</article>
