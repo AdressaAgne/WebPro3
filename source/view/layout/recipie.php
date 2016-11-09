dc38f6f563272cd29a6ad96e0d2c1e30b7fb1387
miatollaksvik <design@miatollaksvik.no>
2016-11-09 13:12:53 +0100

    stuff

--- source/view/layout/recipie.php
+++ source/view/layout/recipie.php
@@ -1,7 +1,7 @@
 <a href="/recipie/item/{{$res->id}}">
 <article class="col-12">
 	<div class="col-12 recipie-preview" style="background-image: url('/assets/img/recipis/{{$res->image}}');"></div>
-	<h2>{{ $res->name}}</h2>
+	<h2 class="medium">{{ $res->name}}</h2>
 
 </article>
 </a>
