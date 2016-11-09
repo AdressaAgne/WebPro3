74c237ac7ef619e353b8a4d41ed97137e76d1669
miatollaksvik <design@miatollaksvik.no>
2016-11-09 12:35:27 +0100

    recipies

--- source/view/recipies.php
+++ source/view/recipies.php
@@ -7,11 +7,48 @@
 
         <h1 class="primary-header-text center">Oppskrifter</h1>
         
-
     </section>
     <div class="container">
+        <div class="col-12" >
+	        <div class="float res-nav">
+		        <p>Nyeste</p>
+		        <p>Best rangert </p>
+		        <p> A - Å </p>
+		       
+	        </div>
+	        <div class="line"></div>
+        </div>
+        
+    <div class="container ">
+    
 	    <div class="col-4">
-	    	search
+	    	<div class="res-search">
+	    	   <input type="search" name="res-search" placeholder="søk i oppskrifter">
+	    	</div>
+	    	<div class="res-categorie">
+	    		<h2> RÅVARE </h2>
+	    		<ul>
+		    		<li><label><input type="checkbox" name="" value="fisk">Fisk</label> </li>
+		    		<li><label><input type="checkbox" name="" value="skalldyr">Skalldyr</label></li>
+		    		<li><label><input type="checkbox" name="" value="urter">Urter</label></li>
+		    		<li><label><input type="checkbox" name="" value="rovdyr">Rovdyr</label></li>
+		    		<li><label><input type="checkbox" name="" value="insekter">Insekter</label></li>
+		    		<li><label><input type="checkbox" name="" value="fruktogberry">Frukt og bær</label></li>
+		    		<li><label><input type="checkbox" name="" value="vegetables">Grønnsaker</label></li>
+		    		<li><label><input type="checkbox" name="" value="poultry">Fjærkre</label></li>
+	    		</ul>
+	    	</div>
+	    	<div class="res-categorie">
+	    		<h2> TYPE RETT </h2>
+	    		<ul>
+	    			<li><label><input type="checkbox" name="" value="frokost">Frokost</label> </li>
+	    			<li><label><input type="checkbox" name="" value="lunsj">Lunsj</label></li>
+	    			<li><label><input type="checkbox" name="" value="middag">Middag</label></li>
+	    			<li><label><input type="checkbox" name="" value="snacks">Snacks</label></li>
+	    			<li><label><input type="checkbox" name="" value="insekter">Insekter</label></li>
+	    			<li><label><input type="checkbox" name="" value="tilbehor">Tilbehør</label></li>
+	    		</ul>
+	    	</div>
 	    </div>
 	    <div class="col-8">
 	        @foreach($food as $res)
