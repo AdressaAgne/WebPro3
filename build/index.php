85075d19958ff1cc23e523b6970ccb192258f7da
miatollaksvik <design@miatollaksvik.no>
2016-11-10 13:35:28 +0100

    deleted build folder

--- build/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-//set_time_limit (6000000);
-
-// The start of everything!
-require_once("App/App.php");
--- build/view/layout/recipie.php
+++
@@ -1,9 +0,0 @@
-<a href="/recipie/item/{{$res->id}}">
-    <article class="col-12">
-        <div class="recipie-preview" style="background-image: url('{{$res->image}}');">
-            
-        </div>
-        <div class="recipie-text">{{ $res->name}}</div>
-
-    </article>
-</a>
