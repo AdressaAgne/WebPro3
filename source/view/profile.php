ac12eef37ddc097fb88dd7fc2166a435938df6ef
lukteperq <grimstadoyvind@gmail.com>
2016-11-14 21:30:28 +0100

    profile ongoing

--- source/view/profile.php
+++ source/view/profile.php
@@ -2,20 +2,32 @@
 
 	<main>
 	    <section class="row primary-header" style="background-image: url('/assets/img/recipis/reindeer_large.jpg')">
-
-            <h1 class="primary-header-text center">{{$user->name}}</h1>
-
+          <h1 class="primary-header-text center">Din Profil</h1>
         </section>
 
-		<section  class="container">
-			<h1 class="page-header center underline">Heisann og skjiit og dra </h1>
-			<div class="col-6 col-l-8 col-m-4 col--center">
-			    <p>
-			      Skjiitsekk
-			    </p>
-			</div>
-		</section>
+  		<section  class="container">
+  			<h1 class="page-header center underline">{{$user->username}}</h1>
+  			<div class="col-6 col-l-8 col-m-4 col--center">
+  			</div>
+        <div class="row">
+          <div class="col-4 col-l-4 col-m-12">
+            <img src="../assets/recipis/banner.jpg" alt="" />
+          </div>
+          <div class="col-8 col-l-8 col-m-12">
+            <img src="../assets/recipis/banner.jpg" alt="" />
+          </div>
 
+          <div class="row">
+            <div class="col-12">
+              @foreach($user->getRecipes() as $recipe)
+
+                  @layout('layout.article_front', ['recipe' => $recipe])
+
+              @endforeach
+            </div>
+          </div>
+        </div><!-- article_front -->
+  		</section>
 	<main>
 
 @layout('layout.foot')
