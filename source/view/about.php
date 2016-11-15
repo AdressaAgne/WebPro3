202b967b2088b61104a79cb617a3452dbc53c7b3
miatollaksvik <design@miatollaksvik.no>
2016-11-15 14:20:01 +0100

    visjon

--- source/view/about.php
+++ source/view/about.php
@@ -1,7 +1,7 @@
 @layout('layout.head')
 
 	<main>
-	    <section class="row primary-header" style="background-image: url('/assets/img/recipis/visjon.jpg')">
+	    <section class="row primary-header" style="background-image: url('/assets/img/visjon/visjon.jpg')">
 
             <h1 class="primary-header-text center">{{$r['name']}}</h1>
    
@@ -9,10 +9,25 @@
 	
 		<section  class="container">
 			<h1 class="page-header center underline"> Dette jobber vi for </h1>
-			<div class="col-6 col-l-8 col-m-4 col--center">
+			<div class="col-6 col-l-8 col-m-4 col--center visjon">
 			    <p>Vår lidenskap er matlaging  og kjærlighet til den norske naturen. Ved å tilby oppskrifter som baserer seg på svartelistede arter hjelper vi den norske befolkningen å være med på forsvare naturen. Disse svartelistede artene er truende for det norske flora og fauna. Eksempler på dette er <a href="#"> Stillehavsøsters </a> og  <a href="#"> Kanadagås. </a> </p>
 			    <p>Dersom vi lykkes med å holde bestanden av disse truende artene nede vil vi opprettholde den norske naturen slik den var ment å være. Dette er bra for både folk, dyr og planter. </p>
 			    <p>Vi samarbeider med en rekke restauranter og andre bidragsytere for å kunne drive dette prosjektet. Vi er avhengig av støtte og så videre. </p>
+			    <p>Les mer om våre samarbeidspartnere her:</p>
+			</div>
+			<div class="col-6 col-l-8 col-m-4 col--center">
+				<div class="partner">
+					<a href="#"><p> Kokken Tor </p>
+					<img src="assets/img/visjon/kokkentor.jpg" /> </a>
+				</div>
+				<div class="partner">
+					<a href="#"><p> Hotell Ball </p>
+					<img src="assets/img/visjon/hotell.jpg" /></a>
+				</div>
+				<div class="partner">
+					<a href="#"><p> Snøgåsrestauranten</p>
+					<img src="assets/img/visjon/restaurant.jpg" /></a>
+				</div>
 			</div>
 		</section>
 	
