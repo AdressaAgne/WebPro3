c0eb60676935a3a0a2c2308ec549d6b26c5b5d9c
miatollaksvik <design@miatollaksvik.no>
2016-11-16 12:32:08 +0100

    fix

--- source/assets/js/artsearch.js
+++ source/assets/js/artsearch.js
@@ -4,7 +4,7 @@ var artsearch = document.getElementById("artsearch");
 
 	artsearch.addEventListener("click", function(){ 	
 	var show = document.getElementById("artsearchfield").style.display;
-	    	if(show == "none"){
+	    	if(show === "none"){
 	    		document.getElementById("artsearchfield").style.display = "inline";
 	    	}else { 
 	    		document.getElementById("artsearchfield").style.display = "none";
