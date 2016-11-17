c69a7da4bd51499aeb7c51eaa39426ccdb8aa91a
thomhess <hessthom@gmail.com>
2016-11-17 15:41:44 +0100

    Searchbar done

--- source/view/index.php
+++ source/view/index.php
@@ -6,17 +6,17 @@
         <div class="background-overlay">
             <div class="row">
                 <div class="col-12">
-                    <i class="icon-logo icon--gigantic icon--center"></i>
+                    <i class="icon-logo--white icon--gigantic icon--center"></i>
 
                     <div class="search col-l-4 col-4 col-m-8 col--center">
                         <label class="search-box" for="search">
                             <input type="search" placeholder="Finn arter nær deg" value="">
                         </label>
-                        <label class="search-icon">
-                            <a href="#"><i class="icon-search--grey icon--small icon--center"></i></a>
+                        <label class="search-icon searchbtn">
+                            <a href="#"><i class="icon-search--theme icon--small icon--center"></i></a>
                         </label>
                         <label class="search-icon geobtn">
-                            <a href="/nearby"><i class="icon-geo icon--small icon--center"></i></a>
+                            <a href="/nearby"><i class="icon-geo--theme icon--small icon--center"></i></a>
                         </label>
 
                         <div class="search-result">
