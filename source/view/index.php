eb91d78d30de7cb703c626853fea837ad67e6d5b
thomhess <hessthom@gmail.com>
2016-11-17 14:44:36 +0100

    Frontpage-stuff

--- source/view/index.php
+++ source/view/index.php
@@ -1,8 +1,8 @@
 @layout('layout.head')
 
 <main>
-    
-    <section class="row primary-header" style="background-image: url('/assets/img/recipis/oyster.jpg')">
+
+    <section class="row primary-header" style="background-image: url('/assets/img/recipis/grilledkingcrab.jpg')">
         <div class="background-overlay">
             <div class="row">
                 <div class="col-12">
@@ -13,37 +13,37 @@
                             <input type="search" placeholder="Finn arter nær deg" value="">
                         </label>
                         <label class="search-icon">
-                            <a href="#"><i class="icon-search--white icon--small icon--center"></i></a>
+                            <a href="#"><i class="icon-search--grey icon--small icon--center"></i></a>
                         </label>
-                        <label class="search-icon">
-                            <a href="/nearby"><i class="icon-geo--white icon--small icon--center"></i></a>
+                        <label class="search-icon geobtn">
+                            <a href="/nearby"><i class="icon-geo icon--small icon--center"></i></a>
                         </label>
 
                         <div class="search-result">
                             <ul id="search-result">
 
-                            </ul>   
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    
+
     <section class="container">
        <h1 class="page-header underline center">Oppskrifter</h1>
        @foreach($food as $recipe)
-       
+
            @layout('layout.article_front', ['recipe' => $recipe])
-       
+
        @endforeach
-       
+
        <h1 class="page-header underline center">Nyheter</h1>
-       
+
        @foreach($food as $recipe)
-       
+
            @layout('layout.article_front', ['recipe' => $recipe])
-       
+
        @endforeach
     </section>
 </main>
