79931478ab3938db047bf7e13f9e03f063906dd4
lukteperq <grimstadoyvind@gmail.com>
2016-11-22 17:29:39 +0100

    infinite loop LOL

--- source/view/error/404.php
+++ source/view/error/404.php
@@ -1,7 +1,7 @@
 @layout('layout.head')
 
 <main>
-    
+
     <section class="row primary-header" style="background-image: url('/assets/img/recipis/oyster.jpg')">
         <div class="background-overlay">
             <div class="row">
@@ -11,17 +11,17 @@
             </div>
         </div>
     </section>
-    
+
     <section>
-       
-        <h1 class="page-header underline center">404 page not found</h1> 
-        
+
+        <h1 class="page-header underline center">404 page not found</h1>
+
         <div class="col-6 col--center">
-            <iframe class="col-12" width="560" height="600" src="https://www.youtube.com/embed/oXYZttxunPw?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1" frameborder="0"></iframe>
+            <iframe class="col-12" width="560" height="600" src="https://www.youtube.com/embed/oXYZttxunPw?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1&loop=1&playlist=oXYZttxunPw" frameborder="0"></iframe>
         </div>
-        
+
     </section>
-    
+
 </main>
 @layout('layout.scripts')
 @layout('layout.foot')
