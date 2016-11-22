95b39f64c96e0e4ee564016caec27629ceccc50d
Agne <agne240@me.com>
2016-11-22 17:20:30 +0100

    wtf parrot 404 page

--- source/view/error/404.php
+++ source/view/error/404.php
@@ -7,8 +7,6 @@
             <div class="row">
                 <div class="col-12">
                     <i class="icon-logo icon--gigantic icon--center"></i>
-
-                    
                 </div>
             </div>
         </div>
@@ -17,6 +15,10 @@
     <section>
        
         <h1 class="page-header underline center">404 page not found</h1> 
+        
+        <div class="col-6 col--center">
+            <iframe class="col-12" width="560" height="600" src="https://www.youtube.com/embed/oXYZttxunPw?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1" frameborder="0"></iframe>
+        </div>
         
     </section>
     
