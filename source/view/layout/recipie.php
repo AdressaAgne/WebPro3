e834dd521bf09db6dc543ca9c7bc7a3a41fa3a81
lukteperq <grimstadoyvind@gmail.com>
2016-12-01 14:14:25 +0100

    Agne pr0fix

--- source/view/layout/recipie.php
+++ source/view/layout/recipie.php
@@ -1,14 +1,7 @@
 <a href="/recipie/item/{{ $res->id }}">
     <div class="recipie-preview" style="background-image: url('{{ $res->thumbnail }}');"></div>
-    <span class="star-rating star-rating--small">
-        <input type="radio" name="rating" value="1" disabled><i></i>
-        <input type="radio" name="rating" value="2" disabled><i></i>
-        <input type="radio" name="rating" value="3" disabled><i></i>
-        <input type="radio" name="rating" value="4" disabled><i></i>
-        <input type="radio" name="rating" value="5" disabled><i></i>
+    <span class="star-rating-fixed">
+      <span class="stars-{{ (int) $res->rating }}"></span>
     </span>
-    <script>
-      document.querySelector('[name=rating]:nth-of-type({{ (int) $res->rating }})').setAttribute('checked', 'checked');
-    </script>
     <div class="recipie-text center">{{ $res->name }}</div>
 </a>
