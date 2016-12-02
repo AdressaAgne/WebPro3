d39fd28ad71928ef92a34e66c376dee504aa0457
miatollaksvik <design@miatollaksvik.no>
2016-12-02 16:06:44 +0100

    Footer

--- source/view/layout/foot.php
+++ source/view/layout/foot.php
@@ -1,6 +1,30 @@
 	<footer class="row">
 	    <div class="container">
-	        footer
+	    	<div class="row">   	
+		    	<div class="footer-info col-4">
+			        <ul>
+			        	<li><h4>Kontakt oss:</h4></li>
+			        	<li>Email: post@farliggodt.no</li>
+						<li>Tlf: 909 09 090  </li>	         
+			        </ul>
+		        </div>
+		        <div class="footer-info col-4">
+			        
+			         <ul>
+			         	<li> <h4>Besøksadresse:</h4></li>
+			        	 <li> Taxon gaten 113 </li>
+			        	 <li> 2828 Artsbyen </li>	         
+			         </ul>
+		         </div>
+		         <div class="footer-info col-4">      
+		              <ul>
+		             	 <li><h4>Følg oss:</h4></li>
+		             	 <li> Facebook</li>
+		             	 <li> Instagram </li>	
+		             	 <li> Snapchat </li>         
+		              </ul>
+		          </div>
+	    	</div>
 	    </div>
 	    <div class="copyright">
 	        <div class="container">
