3e3e9324ab45109ba9a910436dca341858fc2113
miatollaksvik <design@miatollaksvik.no>
2016-12-07 10:49:20 +0100

    Arter layout

--- source/view/layout/taxon.php
+++ source/view/layout/taxon.php
@@ -1,9 +1,10 @@
 <a href="/taxon/item/{{$tax['taxonID']}}">
-    <article class="col-12 taxon-prew">    
+    <article class="col-12 taxon-prew">  
+     <div class="recipie-preview" style="background-image: url('{{$tax['thumbnail']}}');"></div>  
             <div class="content">
                 <h3>{{ ucfirst($tax['navn']) }}</h3>
                 <p><em>{{$tax['scientificName']}}</em></p>
             </div>   
-        <div class="recipie-preview" style="background-image: url('{{$tax['thumbnail']}}');"></div>
+       
     </article>
 </a>
