81dd18dc5a6b8fa046c1f4dd6ec28f2e1e52340a
lukteperq <grimstadoyvind@gmail.com>
2016-12-12 19:44:36 +0100

    admin  = 1

--- source/view/layout/head.php
+++ source/view/layout/head.php
@@ -33,7 +33,7 @@
        @if(Account::isLoggedIn())
 
             <ul class="nav--right">
-              @if($user->getRank() == 4)<!-- If admin -->
+              @if($user->getRank() < 2)<!-- If admin -->
               <li class="nav__item @active_page('admin')">
                   <a href="/admin">Admin</a>
               </li>
